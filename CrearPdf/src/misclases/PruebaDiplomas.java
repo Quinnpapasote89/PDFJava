7f66aa4ca56e7061c65a73d9d5917ddefbb5187b
Quinnpapasote89 <66529547+Quinnpapasote89@users.noreply.github.com>
2023-06-29 09:44:20 -0600

    Add files via upload

---
+++ CrearPdf/src/misclases/PruebaDiplomas.java
@@ -0,0 +1,14 @@
+package misclases;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfImage;
+
+
+/**
+ *
+ * @author Adrian Quinn
+ */
+public class PruebaDiplomas {
+
+}
